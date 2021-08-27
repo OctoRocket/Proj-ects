60b18e2de4fb204ee6b5034a251b09ae99891c56
XTeamXeno <xteamxeno@gmail.com>
2021-08-27 17:22:18 -0500

    moved

--- Applesoft-BASIC/Fibbonacci.py
+++ Applesoft-BASIC/Fibbonacci.py
@@ -1,16 +1,15 @@
-# x = 0
-# while True:
-#     sleep(1)
-#     print(x)
-#     y = x
-#     if x == 0:
-#         x = 1
-#     x = x + y
-
-n1=0
-n2=1
+import time
+x = 0
+a = 0
 while True:
-    print(n1)
-    tmp=n1+n2
-    n1=n2
-    n2=tmp
+    print(x)
+    b = x
+    if x == 0:
+        x = 1
+    elif a == 0:
+        a = 1
+    elif a == 1:
+        y = x
+        z = b
+        x = y + z
+    time.sleep(0.5)
