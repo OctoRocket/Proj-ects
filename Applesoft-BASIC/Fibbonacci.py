8bd9317975e17c1f48702235f839a4159fa19990
XTeamXeno <xteamxeno@gmail.com>
2021-08-27 16:27:55 -0500

    fix fib test (to show xoplex9000)

--- Applesoft-BASIC/Fibbonacci.py
+++ Applesoft-BASIC/Fibbonacci.py
@@ -1,7 +1,18 @@
-x = 0
+from time import sleep
+# x = 0
+# while True:
+#     sleep(1)
+#     print(x)
+#     y = x
+#     if x == 0:
+#         x = 1
+#     x = x + y
+
+n1=0
+n2=1
 while True:
-    print(x)
-    y = x
-    if x == 0:
-        x = 1
-    x = x + y
+    print(n1)
+    tmp=n1+n2
+    n1=n2
+    n2=tmp
+    sleep(1)
