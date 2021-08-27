c67e18a945406df7277bc0430d6582e0d05038be
XTeamXeno <xteamxeno@gmail.com>
2021-08-27 17:25:15 -0500

    Fix fib 2.0\

--- Applesoft-BASIC/Fibbonacci.py
+++ Applesoft-BASIC/Fibbonacci.py
@@ -1,15 +1,9 @@
 import time
-x = 0
-a = 0
+number_1 = 0
+number_2 = 1
 while True:
-    print(x)
-    b = x
-    if x == 0:
-        x = 1
-    elif a == 0:
-        a = 1
-    elif a == 1:
-        y = x
-        z = b
-        x = y + z
-    time.sleep(0.5)
+    print(number_1)
+    number_1_plus_number_2 = number_1 + number_2
+    number_1 = number_2
+    number_2 = number_1_plus_number_2
+    time.sleep(1)
