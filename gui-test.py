1dccdeb82d5351371b7468f9fa9922b0adbae06c
OctoRocket <xteamxeno@gmail.com>
2021-08-28 14:33:28 -0500

    gui-test update, unfinished

--- gui-test.py
+++ gui-test.py
@@ -1,26 +1,7 @@
-import tkinter as tk
+from tkinter import *
 
-class Application(tk.Frame): #Create a class named Application with the variable tk.Frame
-                             #which (I am kinda sure) is to make the border around the window
-    def __init__(self, master=None): #This runs when you run application()
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+window = Tk()
+window.title("gui-test")
+photo1 = PhotoImage(file="1.png")
+Label (window, image=photo1, bg="black",) .grid(row=0, column=0, sticky=E)
+window.mainloop()
