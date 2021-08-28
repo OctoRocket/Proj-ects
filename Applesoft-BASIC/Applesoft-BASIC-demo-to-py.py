3f1edc310223289b091bf4e20fc3887584a9b627
Sussy Baka <xoplex9000@gmail.com>
2021-08-28 17:39:00 -0500

    Guess your number

--- Applesoft-BASIC/Applesoft-BASIC-demo-to-py.py
+++ Applesoft-BASIC/Applesoft-BASIC-demo-to-py.py
@@ -92,6 +92,28 @@ def basic_io_2():
         print("FALSE")
     print()
     input("Press ENTER")
+def guess_your_number_5():
+    min_guess = 1
+    max_guess = 100
+    while True:
+        try:
+            number = int(input("Pick a number from 1 to 100, then press Enter: "))
+        except ValueError:
+            print("?REENTER")
+        while True:
+            guess=random.randint(min_guess,max_guess)
+            print("I guessed:",guess)
+            response = input("Enter 1 for too high, -1 for too low, and 0 for got it ")
+            if response == "1":
+                max_guess = guess-1
+                print("ok")
+            elif response == "-1":
+                min_guess = guess+1
+                print("ok")
+            elif response == "0":
+                print("Yay!")
+                input("Press ENTER")
+                exit("Won")
 def element_choice():
     print("(1) GOSUB/RETURN/POP")
     print("(2) Basic I/O, IF/THEN")
