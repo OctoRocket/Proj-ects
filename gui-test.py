60745127c8d8ed99c3091dbcecba0231e60d94b8
OctoRocket <xteamxeno@gmail.com>
2021-08-28 09:04:38 -0500

    fardor

---
+++ gui-test.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+class Application(tk.Frame): #Create a class named Application with the variable tk.Frame
+                             #which (I am kinda sure) is to make the border around the window
+    def __init__(self, master=None): #This runs when you run application()
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
