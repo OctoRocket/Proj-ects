7828dac1c6fb1665b632ae86264477f816234466
OctoRocket <xteamxeno@gmail.com>
2021-08-29 08:58:06 -0500

    Maybe fixed gui on mac?

--- gui-test.py
+++ gui-test.py
@@ -2,6 +2,6 @@ from tkinter import *
 
 window = Tk()
 window.title("gui-test")
-photo1 = PhotoImage(file="1.png")
+photo1 = PhotoImage(file="./1.png")
 Label (window, image=photo1, bg="black",) .grid(row=0, column=0, sticky=E)
 window.mainloop()
